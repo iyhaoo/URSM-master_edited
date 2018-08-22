7abac552b263d00403e50b5fe255743ff990ef54
iyhaoo <tyhaoo@gmail.com>
2018-08-22 09:12:20 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# URSM-master_edited
---
+++ run_scunif.sh
@@ -0,0 +1,23 @@
+
+data_dir=/home/yuanhao/single_cell/impute_criteria/simulated_data
+output_dir=/home/yuanhao/single_cell/impute_criteria/outputs/scUnif
+log_dir=/home/yuanhao/single_cell/impute_criteria/outputs/scUnif_log
+mkdir -p $output_dir
+
+
+dataset=$(ls $data_dir | grep -P "dataset.+?dropout\\.mtx")
+
+for ii in $dataset
+do
+dataset_name=${ii%_dropout.mtx}
+python3 /home/yuanhao/single_cell/impute_criteria/imputation_methods/URSM-master_edited/scUnif.py \
+	-sc $data_dir"/"$ii \
+	-ctype $data_dir"/"$dataset_name"_groups.tsv" \
+	-outdir $output_dir \
+	--output_prefix $dataset_name \
+	-log $log_dir/logging_file.log \
+	-burnin 50 \
+	-sample 50 \
+	-EM_maxiter 50
+done
+
